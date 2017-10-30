b9131ad61a82f2fac685bf5ae937e8518827ca67
Francisco Carrera <franciscocarrer@gmail.com>
2017-10-30 00:11:50 -0400

    Adjusted routes as placeholders for current one page only display

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(request, response){
-    response.render("index");
+    response.render("about");
 });
 
 router.get("/about", function (req, res) {
